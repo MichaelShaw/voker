27805416d6b7c856ba3540be18fcf77a96fad501
Michael Shaw <michaeljshaw@gmail.com>
2017-08-03 16:52:08 +1000

    Lexing WIP

--- templar/src/lex.rs
+++ templar/src/lex.rs
@@ -3,18 +3,7 @@ use nom::*; // {digit, space, alphanumeric}
 use std::str;
 
 
-// tokens
-#[derive(Debug, PartialEq, Eq)]
-pub enum Token {
-    Indentation(u32),
-    Element(String),
-    ElementId(String),
-    ElementClass(String),
-    Text(String),
-    Directive(Vec<String>),
-}
-
-pub type LexResult = Result<Vec<Vec<Token>>, LexError>;
+pub type LexResult = Result<Vec<Line>, LexError>;
 
 #[derive(Debug)]
 pub struct LexError {
@@ -35,7 +24,17 @@ fn character_length(bytes: &[u8]) -> Result<usize, str::Utf8Error> {
     Ok(st.len())
 }
 
+fn to_str(v:Vec<char>) -> String {
+    v.into_iter().collect()
+}
+
+fn is_identifier(b:u8) -> bool {
+    let c = b as char;
+    c.is_alphanumeric() || c == '-'
+}
+
 // captures indentation length in characters (for tracking nesting)
+// need to handle zero indentation
 named!(indentation<usize>,
     map_res!(
         space,
@@ -43,13 +42,9 @@ named!(indentation<usize>,
     )
 );
 
-named!(line_p<Option<usize>>,
-    opt!(indentation)
-);
-
 named!(identifier<&str>,
     map_res!(
-        alphanumeric,
+        take_while1!(is_identifier),
         str::from_utf8
     )
 );
@@ -73,7 +68,7 @@ named!(rest<&str>,
 named!(text<&str>,
     do_parse!(
         tag!("|") >>
-        rr: rest >>
+        rr: map!(rest, |s| s.trim() ) >>
         (rr)
     )
 );
@@ -94,6 +89,64 @@ named!(directive<&str>,
     )
 );
 
+named!(text_line<LineContent>,
+    map!(
+        text,
+        |s| LineContent::Text(s.to_string())
+    )
+);
+
+named!(directive_line<LineContent>,
+    map!(
+        directive,
+        |s| LineContent::Directive(s.to_string())
+    )
+);
+
+named!(empty_line<LineContent>,
+    do_parse!(
+        many0!(space) >>
+        eof!() >>
+        ( LineContent::Empty )
+    )
+);
+
+named!(line_p<Line>,
+    do_parse!(
+        indent: opt!(indentation) >>
+        line_content: alt!(text_line | directive_line | empty_line) >>
+
+        ( Line { indentation: indent.unwrap_or(12), content: line_content } )
+    )
+);
+
+#[derive(Debug)]
+pub enum ClassId {
+    Id(String),
+    Class(String),
+}
+
+#[derive(Debug)]
+pub struct Element {
+    tag: Option<String>,
+    stuff: Vec<ClassId>,
+    inner_text: Option<String>,
+}
+
+#[derive(Debug)]
+pub enum LineContent {
+    Element(String),
+    Directive(String),
+    Text(String),
+    Empty,
+}
+
+#[derive(Debug)]
+pub struct Line {
+    pub indentation: usize,
+    pub content: LineContent,
+}
+
 pub fn lex(content:&str) -> LexResult {
 
     println!("lexing content length: {:?} ", content.len());
@@ -103,7 +156,12 @@ pub fn lex(content:&str) -> LexResult {
     for (line_idx, line) in lines {
         println!("{:?}: in -> {:?}", line_idx, line);
         let line_result = line_p(line.as_bytes());
-        println!("{:?}: out -> {:?}", line_idx, line_result);
+        match line_result {
+            IResult::Done(i, o) => println!("Done-> {:?}", o),
+            IResult::Error(err) => println!("Err -> {:?}", err),
+            IResult::Incomplete(needed) => println!("Incomplete -> {:?}", needed),
+        }
+
 
     }
 
